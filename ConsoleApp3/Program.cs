1bc7d3dbf4b1f45c09e5a011a3e4dc410f817c9f
YakirBarzily <notcooldudet@gmail.com>
2022-02-06 18:22:32 +0200

    Shabatch

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter you child age");
+            int counter1;
+            int age;
+            for (int i = 0; i < 50; i++)
+            {
+                age = int.Parse(Console.ReadLine());
+                if (age > 12 && age < 15)
+                    counter1++;
+            }
+        }
+    }
+}
